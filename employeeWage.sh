e4183fbf31c1a55f73a02a13303516bc8ac6eee8
ARATI-UPARE <aratiupare@gmail.com>
2020-03-11 17:40:54 +0530

    ADDED: employeeWage

--- employeeWage.sh
+++ employeeWage.sh
@@ -3,11 +3,11 @@
 
 isPresent=1
 randomCheck=$((RANDOM%2))
-	
+
 if [ $isPresent -eq $randomCheck ]
 then
 		empRatePerHr=20
-		empHrs=8;
+		empHrs=8
 		salary=$(( $empHrs * $empRatePerHr ))
 		echo Employee wage is $salary
 else
